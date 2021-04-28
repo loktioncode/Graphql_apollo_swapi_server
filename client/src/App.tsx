946d993158f84a3ca343d30820a724166bd5f8d0
DESKTOP-TEK06LE\EBere <elisha.b@cestasoft.co.za>
2021-04-28 18:15:28 +0200

    add person and people components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function StarWarsApp() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React TypeScript
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default StarWarsApp;
